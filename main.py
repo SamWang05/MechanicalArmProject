2f8df475e62a090e1f583cdf59811fb8c5869495
SamWang05 <wangsam00207@gmail.com>
2026-06-26 01:21:52 -0400

    Updated initialize library

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from scripts.lib import initialize as init
 
 servoHip = init.initializeHip()
+servoShoulder = init.initializeShoulder()
+servoUpperElbow = init.initializeUpperElbow()
+servoLowerElbow = init.initializeLowerElbow()
+servoWrist = init.initializeWrist()
+servoHand = init.initializeHand()
 
 servoHip.printServoProperties()
+servoShoulder.printServoProperties()
+servoUpperElbow.printServoProperties()
+servoLowerElbow.printServoProperties()
+servoWrist.printServoProperties()
+servoHand.printServoProperties()
--- scripts/lib/initialize.py
+++ scripts/lib/initialize.py
@@ -9,7 +9,7 @@ config.read('scripts/configs/inis/armConfig.ini')
 
 
 def initializeHip():
-    servoHip = sPos.servoProperties(
+    hipServo = sPos.servoProperties(
         config['HIP']['Name'],
         int(config['HIP']['IDNum']),
 
@@ -19,10 +19,99 @@ def initializeHip():
         float(config['HIP']['ServoMinPosition']),
         float(config['HIP']['ServoMaxPosition']),
 
-        11.1, # Servo Voltage -> Replace with data from serial read
+        11.6, # Servo Voltage -> Replace with data from serial read
         27.3, # Servo Temp -> Replace with data from serial read
 
         config['HIP'].getboolean('ServoLockStatus'),
     )
+    return hipServo
 
-    return servoHip
+def initializeShoulder():
+    shoulderServo = sPos.servoProperties(
+        config['SHOULDER']['Name'],
+        int(config['SHOULDER']['IDNum']),
+
+        float(config['SHOULDER']['ServoTargetPosition']),
+        24.7, # Actual Angle Position -> Replace with data from serial read
+
+        float(config['SHOULDER']['ServoMinPosition']),
+        float(config['SHOULDER']['ServoMaxPosition']),
+
+        11.9, # Servo Voltage -> Replace with data from serial read
+        33.3, # Servo Temp -> Replace with data from serial read
+
+        config['SHOULDER'].getboolean('ServoLockStatus'),
+    )
+    return shoulderServo
+
+def initializeUpperElbow():
+    upperElbowServo = sPos.servoProperties(
+        config['UPPER ELBOW']['Name'],
+        int(config['UPPER ELBOW']['IDNum']),
+
+        float(config['UPPER ELBOW']['ServoTargetPosition']),
+        105.6, # Actual Angle Position -> Replace with data from serial read
+
+        float(config['UPPER ELBOW']['ServoMinPosition']),
+        float(config['UPPER ELBOW']['ServoMaxPosition']),
+
+        12.5, # Servo Voltage -> Replace with data from serial read
+        99.2, # Servo Temp -> Replace with data from serial read
+
+        config['UPPER ELBOW'].getboolean('ServoLockStatus'),
+    )
+    return upperElbowServo
+
+def initializeLowerElbow():
+    lowerElbowServo = sPos.servoProperties(
+        config['LOWER ELBOW']['Name'],
+        int(config['LOWER ELBOW']['IDNum']),
+
+        float(config['LOWER ELBOW']['ServoTargetPosition']),
+        45.8, # Actual Angle Position -> Replace with data from serial read
+
+        float(config['LOWER ELBOW']['ServoMinPosition']),
+        float(config['LOWER ELBOW']['ServoMaxPosition']),
+
+        10.4, # Servo Voltage -> Replace with data from serial read
+        60.7, # Servo Temp -> Replace with data from serial read
+
+        config['LOWER ELBOW'].getboolean('ServoLockStatus'),
+    )
+    return lowerElbowServo
+
+def initializeWrist():
+    wristServo = sPos.servoProperties(
+        config['WRIST']['Name'],
+        int(config['WRIST']['IDNum']),
+
+        float(config['WRIST']['ServoTargetPosition']),
+        134.5, # Actual Angle Position -> Replace with data from serial read
+
+        float(config['WRIST']['ServoMinPosition']),
+        float(config['WRIST']['ServoMaxPosition']),
+
+        10.3, # Servo Voltage -> Replace with data from serial read
+        27.0, # Servo Temp -> Replace with data from serial read
+
+        config['WRIST'].getboolean('ServoLockStatus'),
+    )
+    return wristServo
+
+def initializeHand():
+    handServo = sPos.servoProperties(
+        config['HAND']['Name'],
+        int(config['HAND']['IDNum']),
+
+        float(config['HAND']['ServoTargetPosition']),
+        118.3, # Actual Angle Position -> Replace with data from serial read
+
+        float(config['HAND']['ServoMinPosition']),
+        float(config['HAND']['ServoMaxPosition']),
+
+        11.5, # Servo Voltage -> Replace with data from serial read
+        89.6, # Servo Temp -> Replace with data from serial read
+
+        config['HAND'].getboolean('ServoLockStatus'),
+    )
+    return handServo
