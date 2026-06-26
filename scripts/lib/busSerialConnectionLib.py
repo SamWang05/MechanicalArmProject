5c5b98077be26e42fd45b0ef755792b9a6f50bcf
SamWwang05 <wangsam00207@gmail.com>
2026-06-25 22:33:06 -0400

    misc. changes - busSerialConnLib

--- scripts/lib/busSerialConnectionLib.py
+++ scripts/lib/busSerialConnectionLib.py
@@ -6,12 +6,11 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import time
 import serial
 
 # Currently testing to see if serial connection can be established
 
-serialPort = serial.Serial('/dev/ttyUSB0', 9600)
+serialPort = serial.Serial('/dev/ttyUSB0', 9600, timeout = 10)
 
 print(serialPort.name)
 print("Successfully printed serial port name")
