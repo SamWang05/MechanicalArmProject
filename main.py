ed96f48d633252425d5a1b3c92143c6a384ecc24
SamWang05 <wangsam00207@gmail.com>
2026-07-15 19:24:39 -0400

    Added notes on software redesign

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# Rename based on new mechanical design, TBA
+
 from lib import initializeArmPosLib as initAP
 from lib import servoPosLib as sPos
 
